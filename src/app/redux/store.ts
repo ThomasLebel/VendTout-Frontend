a89fa256e784ff0757ceb7f0ca28e619cbe0dfd4
Thomas Lebel <thomas.lebel38@gmail.com>
2025-03-11 11:06:37 +0100

    Ajout persistore

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -2,14 +2,46 @@ import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import userReducer from "./slices/userSlice";
 import messageInformationReducer from "./slices/messageInformation";
+import {
+  persistStore,
+  persistReducer,
+  PERSIST,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+
+// Configuration de redux-persist
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["nonSerializableReducer"],
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  messageInformation: messageInformationReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 //Création du store
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    messageInformation: messageInformationReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+// Persistance du store
+export const persistor = persistStore(store);
 
 //Définition  ds types pour typescript
 export type RootState = ReturnType<typeof store.getState>;
